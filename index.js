df1c5e6c63c440b15a46d890da970a7a9f142790
Mike Leach <mikecleach@gmail.com>
2019-07-18 15:02:07 -0500

    actually using typescipt now. initial typeorm install

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-
+"use strict";
+exports.__esModule = true;
+//const express = require("express");
+var express_1 = require("express");
+var path = require("path");
+var app = express_1["default"]();
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, "client/build")));
-
+app.use(express_1["default"].static(path.join(__dirname, "client/build")));
 // Put all API endpoints under '/api'
-app.get("/api/user", (req, res) => {
-    const obj = { hello: "world" };
-
+app.get("/api/user", function (req, res) {
+    var obj = { hello: "world" };
     // Return them as json
     res.json(obj);
-
-    console.log(`Sent object`);
+    console.log("Sent object");
 });
-
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get("*", (req, res) => {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
-
-const port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log(`Password generator listening on ${port}`);
+console.log("Password generator listening on " + port);
---
+++ index.ts
@@ -0,0 +1,30 @@
+//const express = require("express");
+import express from "express";
+const path = require("path");
+//import path from "path";
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// Put all API endpoints under '/api'
+app.get("/api/user", (req, res) => {
+    const obj = { hello: "world" };
+
+    // Return them as json
+    res.json(obj);
+
+    console.log(`Sent object`);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
