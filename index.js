f9bb1d6421048163934bd37335c5589c7fb63749
Mike Leach <mikecleach@gmail.com>
2019-07-17 20:13:58 -0500

    created react app in client dir

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const PORT = process.env.PORT || 5000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// Put all API endpoints under '/api'
+app.get("/api/user", (req, res) => {
+    const count = 5;
+
+    // Generate some passwords
+    const passwords = Array.from(Array(count).keys()).map(i => generatePassword(12, false));
+
+    // Return them as json
+    res.json(passwords);
+
+    console.log(`Sent ${count} passwords`);
 });
-server.listen(PORT, () => {
-    console.log(`Server running on ${PORT}/`);
-  });
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
