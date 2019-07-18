4e521767c4b914490a8c530846e587a2ed5eaa8c
Mike Leach <mikecleach@gmail.com>
2019-07-18 16:06:38 -0500

    actually providing a port now

--- index.ts
+++ index.ts
@@ -5,6 +5,6 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("The sedulous hyena ate the antelope!");
 });
-app.listen(port => {
-    return console.log(`server is listening on ${port}`);
-});
+app.listen(port);
+
+console.log(`listening on port ${port}`);
