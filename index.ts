424cce16903402a289532d22a6a5367ca81949d1
Mike Leach <mikecleach@gmail.com>
2019-07-18 15:53:31 -0500

    using heroku assigned port now

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("The sedulous hyena ate the antelope!");
 });
-app.listen(port, err => {
-    if (err) {
-        return console.error(err);
-    }
+app.listen(port => {
     return console.log(`server is listening on ${port}`);
 });
