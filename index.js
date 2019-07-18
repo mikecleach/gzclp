e8d4198ed808a895a204f99fe103d1e3936013c2
Mike Leach <mikecleach@gmail.com>
2019-07-17 20:20:08 -0500

    testing heroku routing

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ app.use(express.static(path.join(__dirname, "client/build")));
 
 // Put all API endpoints under '/api'
 app.get("/api/user", (req, res) => {
-    const count = 5;
-
-    // Generate some passwords
-    const passwords = Array.from(Array(count).keys()).map(i => generatePassword(12, false));
+    const obj = { hello: "world" };
 
     // Return them as json
-    res.json(passwords);
+    res.json(obj);
 
-    console.log(`Sent ${count} passwords`);
+    console.log(`Sent object`);
 });
 
 // The "catchall" handler: for any request that doesn't
