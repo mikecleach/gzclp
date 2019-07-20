667fa5bbc0f39bd1c27d62789e6bcbc5e8e8b169
Mike Leach <mikecleach@gmail.com>
2019-07-20 17:20:56 -0500

    save user api added

--- index.ts
+++ index.ts
@@ -1,14 +1,28 @@
 import express from "express";
 
-import { createConnection } from "typeorm";
+import { createConnection, getRepository } from "typeorm";
 import { getOrmConfig } from "./ormconfig";
+
+import { User } from "./src/entity/User";
 
 //TODO: move express start into connection's .then
 const ormConfig = getOrmConfig();
 const dbConn = createConnection(ormConfig).catch(err => console.log("typeorm connection error:" + err));
 
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT || 5000;
+app.post("/user", (req, res) => {
+    let newUser = new User();
+    newUser.login = req.body.login;
+    newUser.email = req.body.email;
+
+    getRepository(User).save(newUser);
+
+    res.send("saved user? " + newUser);
+});
+
 app.get("/", (req, res) => {
     res.send("The sedulous hyena ate the antelope!");
 });
