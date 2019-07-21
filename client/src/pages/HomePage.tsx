c84dec65d62f8ff2c060df98e43fdc154941be94
Mike Leach <mikecleach@gmail.com>
2019-07-21 00:53:56 -0500

    first react component, homepage

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h2>Welcome to GZCLP Tracker</h2>
+            <div>
+                <p>Please login or signup</p>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
